e1d6f4250e416e52ff1358eab1e7d94a7341deb0
jyz0501 <jyz_0501@live.cn>
2024-03-24 18:17:18 +0800

    Update sensor.py

--- custom_components/zinguo/sensor.py
+++ custom_components/zinguo/sensor.py
@@ -1,43 +1,42 @@
 """
 Zinguo platform for sensors
 """
-import json
-import time
 import asyncio
-from homeassistant.components.sensor import (ENTITY_ID_FORMAT, PLATFORM_SCHEMA)
-from homeassistant.helpers.entity import Entity
-from homeassistant.helpers.dispatcher import (dispatcher_connect, dispatcher_send)
-from homeassistant.const import (DEVICE_CLASS_TEMPERATURE, TEMP_CELSIUS, CONF_NAME, CONF_MAC, CONF_TOKEN, CONF_USERNAME)
-import homeassistant.helpers.config_validation as cv
-from custom_components.zinguo.const import *
-import voluptuous as vol
-from homeassistant.util import  slugify
 import logging
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
+from homeassistant.const import (
+    DEVICE_CLASS_TEMPERATURE,
+    TEMP_CELSIUS,
+)
+from homeassistant.util import slugify
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from custom_components.zinguo.const import *
 
 _LOGGER = logging.getLogger(__name__)
 
-deviceType = [CONF_TEMPERATURE]
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_TEMPERATURE): cv.string,
+})
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the ZiGate sensors."""
     devices = []
-    for devType in deviceType:
-        devices.append(ZinguoSensor(hass, config.get(devType), devType))
+    for devType, name in config.items():
+        if devType == CONF_TEMPERATURE:
+            devices.append(ZinguoSensor(hass, name, devType))
 
-    add_devices(devices)
+    async_add_entities(devices)
 
-
-class ZinguoSensor(Entity):
+class ZinguoSensor(SensorEntity):
     def __init__(self, hass, name, devType):
         """Initialize the sensor."""
-        self._hass = hass
-        self.entity_id = ENTITY_ID_FORMAT.format(slugify(devType))
+        self.entity_id = f"sensor.{slugify(name)}"
         self._name = name
         self._type = devType
         self._state = None
         self._attributes = {}
-        hass.bus.listen(CONF_EVENT_ZINGUO_SENSOR, self._handle_event)
-
 
     @property
     def name(self):
@@ -48,23 +47,24 @@ class ZinguoSensor(Entity):
     def state(self):
         """Return the state of the sensor."""
         return self._state
+
     @property
     def device_class(self):
         """Return the device class of this entity."""
         return DEVICE_CLASS_TEMPERATURE
 
-    def _handle_event(self, event):
-        """eventMsg = json.loads(event.data)"""
+    async def async_added_to_hass(self):
+        """Subscribe to updates."""
+        async_dispatcher_connect(self.hass, CONF_EVENT_ZINGUO_SENSOR, self._handle_event)
+
+    async def _handle_event(self, event):
+        """Handle incoming events."""
         eventMsg = event.data
-        _LOGGER.debug('ZINGUO : handle event start')
         if self._type in eventMsg:
             self._state = eventMsg[self._type]
-            _LOGGER.debug('ZINGUO : self._state %s', self._state)
-            #self.schedule_update_ha_state()
+            self.async_write_ha_state()
 
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity, if any."""
         return TEMP_CELSIUS
-
-
